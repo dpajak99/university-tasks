60368a582d3604453c7aa8738df9fb8a9c9035e9
Dominik Pająk <dominik00801@gmail.com>
2020-10-19 11:42:16 +0200

    Initial commit

---
+++ tgryl/ppsm_lab_3/app/src/main/java/com/softarea/ppsm_lab_3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.softarea.ppsm_lab_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
