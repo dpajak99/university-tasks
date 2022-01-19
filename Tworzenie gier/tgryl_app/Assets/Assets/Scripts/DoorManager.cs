5a967e118c39809cc9198785d72d32c2178aa0f7
Dominik <dominik00801@gmail.com>
2022-01-19 19:09:06 +0100

    tgryl_lab04

---
+++ Tworzenie gier/tgryl_app/Assets/Assets/Scripts/DoorManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour {
+    bool doorIsOpen = false;
+    float doorTimer = 0.0f;
+    public float doorOpenTime = 3.0f;
+    public AudioClip doorOpenSound;
+    public AudioClip doorShutSound;
+    
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(doorIsOpen){
+	    doorTimer += Time.deltaTime;
+	    if(doorTimer > doorOpenTime){
+                Door(doorShutSound, false, "doorshut");
+                doorTimer = 0.0f;
+            }
+        }
+    }
+    
+    void DoorCheck(){
+        if(!doorIsOpen){
+            Door(doorOpenSound, true, "dooropen");
+        }
+    }
+    
+    void Door(AudioClip aClip, bool openCheck, string animName){
+        doorIsOpen = openCheck;
+	this.GetComponent<AudioSource>().PlayOneShot(aClip);
+	this.transform.parent.GetComponent<Animation>().Play(animName);
+    }
+}
--- Tworzenie gier/tgryl_app/Assets/Assets/Scripts/PlayerCollisions.cs
+++ Tworzenie gier/tgryl_app/Assets/Assets/Scripts/PlayerCollisions.cs
@@ -3,31 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCollisions : MonoBehaviour {
-    bool doorIsOpen = false;
-    float doorTimer = 0.0f;
-    public float doorOpenTime = 3.0f;
-    public AudioClip doorOpenSound;
-    public AudioClip doorShutSound;
+    GameObject currentDoor;
     
-    // Start is called before the first frame update
     void Start() {
         
     }
 
-    // Update is called once per frame
     void Update() {
-        
-    }
-    
-    void OnControllerColliderHit(ControllerColliderHit hit){
-        if(hit.gameObject.tag == "playerDoor" && doorIsOpen == false){
-            OpenDoor(hit.gameObject);
+       RaycastHit hit;
+        if(Physics.Raycast (transform.position, transform.forward, out hit, 3)) {
+            if(hit.collider.gameObject.tag=="playerDoor"){
+                currentDoor = hit.collider.gameObject;
+                currentDoor.SendMessage("DoorCheck");
+            }
         }
     }
-    
-    void OpenDoor(GameObject door){
-        doorIsOpen = true;
-        door.GetComponent<AudioSource>().PlayOneShot(doorOpenSound);
-        door.transform.parent.GetComponent<Animation>().Play("dooropen");
-    }
 }
+    
